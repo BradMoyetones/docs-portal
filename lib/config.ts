c1aafc36a67354e68b3f0a95b5d8e8509a5c7526
BradMoyetones <brad.moyetones@gmail.com>
2025-10-21 15:09:22 -0500

    first commit

---
+++ lib/config.ts
@@ -0,0 +1,42 @@
+export const siteConfig = {
+  name: "Parque Jaime Duque",
+  url: "https://docs.portaljaimeduque.net",
+  ogImage: "https://docs.portaljaimeduque.net/og.jpg",
+  description:
+    "Documentación de cada uno de los proyectos desarrollados en el Parque Jaime Duque.",
+  links: {
+    twitter: "https://twitter.com/ParqJaimeduque",
+    github: "https://github.com/BradParqueJaimeDuque",
+  },
+  navItems: [
+    {
+      href: "/docs/installation",
+      label: "Docs",
+    },
+    {
+      href: "/docs/components",
+      label: "Components",
+    },
+    {
+      href: "/blocks",
+      label: "Blocks",
+    },
+    {
+      href: "/charts/area",
+      label: "Charts",
+    },
+    {
+      href: "/themes",
+      label: "Themes",
+    },
+    {
+      href: "/colors",
+      label: "Colors",
+    },
+  ],
+}
+
+export const META_THEME_COLORS = {
+  light: "#ffffff",
+  dark: "#09090b",
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+import { createMDX } from "fumadocs-mdx/next"
 import type { NextConfig } from "next";
+
+const withMDX = createMDX({
+  // customise the config file path
+  // configPath: "source.config.ts"
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
